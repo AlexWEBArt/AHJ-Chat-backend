1720bf663cd5d7f06b372e358d91b9015aecb2d2
Maksim Kvashnin <kvashnin@hotmail.com>
2022-06-01 16:16:47 +0700

    На конец урока 2. Fetch

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 // TODO: write code here
 
-(async () => {
-  const request = fetch('http://localhost:7070/index');
+class SubsctiptionApi {
+  constructor(apiUrl) {
+    this.apiUrl = apiUrl;
+  }
+  
+  async add(user) {
+    const request = fetch(this.apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user),
+    });
+    
+    const result = await request;
+    
+    if (!result.ok) {
+      console.error('Ошибка');
+      
+      return;
+    }
 
-  const result = await request;
+    const json = await result.json();
+    const status = json.status;
+    
+    console.log(status);
+  }
+  
+  async remove(user) {
+    const query = '/?phone=' + user.phone;
 
-  sonst text = await result.text();
-})();
+    const request = fetch(this.apiUrl + query, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user),
+    });
+    
+    const result = await request;
+    const json = await result.json();
+    
+    const status = json.status;
+    
+    console.log(status);
+  }
+}
+
+window.api = new SubscriptionApi('http://localhost:7070/');
 
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const http = require('http');
 const Koa = require('koa');
 const Router = require('koa-router');
+const koaBody = require('koa-body');
 const app = new Koa();
+
+app.use(koaBody({
+  urlencoded: true,
+}));
 
 app.use((ctx, next) => {
   const origin = ctx.request.get('Origin');
@@ -35,6 +40,63 @@ app.use((ctx, next) => {
   }
 });
 
+let subcriptions = [];
+
+app.use((ctx, next) => {
+  if (ctx.request.method !== 'POST') {
+    next();
+    
+    return;
+  }
+  
+  console.log(typeof ctx.request.body);
+  console.log(ctx.request.body);
+  
+  const { name, phone } = ctx.request.body;
+  
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+  
+  if (subscription.some(sub => sub.phone === phone)) {
+    ctx.response.status = 400;
+    ctx.response.body = '{ "status": "subscriprion exists" }';
+    
+    return;
+  }
+  
+  subscriptions.push({ name, phone });
+  
+  ctx.response.body = '{ "status": "OK" }';
+  
+  next();
+});
+
+app.use((ctx, next) => {
+  if (ctx.request.method !== 'DELETE') {
+    next();
+    
+    return;
+  }
+  
+  console.log(ctx.request.query);
+  
+  const { phone } = ctx.request.query;
+  
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+  
+  if (subsriptions.every(sub => sub.phone !== phone)) {
+    ctx.response.status = 400;
+    ctx.response.body = '{ "status": "subscriprion doesn\'t exists" }';
+    
+    return;
+  }
+  
+  subscriptions = subscriptions.filter(sub => sub.phone !== phone);
+  
+  ctx.response.body = '{ "status": "OK" }';
+  
+  next();
+});
+
 const router = new Router();
 
 //TODO: write code here
@@ -48,4 +110,3 @@ app.use(router.routes()).use(router.allowedMethods());
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback());
 server.listen(port);
-
